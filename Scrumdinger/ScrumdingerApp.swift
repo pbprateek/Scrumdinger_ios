ae0f2298e24499674259dde3d610072f6f436b19
Prateek <prateek2109@outlook.com>
2022-03-11 18:19:10 +0530

    Meeting View

--- Scrumdinger/ScrumdingerApp.swift
+++ Scrumdinger/ScrumdingerApp.swift
@@ -7,16 +7,17 @@
 
 import SwiftUI
 
+
+//@main and confirm to App protocol to Tell what is starting point
 @main
 struct ScrumdingerApp: App {
     
     @State private var scrums = DailyScrum.sampleData
     
     
-
-    
-    
+    //In ios we only add one scene but in pad or mac there can be multiple
     var body: some Scene {
+        //SwiftUI provides primitive scenes like WindowGroup and system manages lifecycle of these Scenes
         WindowGroup {
             //Still feel NavigationView should be used inside ScrumView
             NavigationView{
